1f408a820295a905725c01ffecc66924b30ecd10
Stuart Urback <stuart@urback.net>
2016-02-27 23:02:13 -0700

    Add basic local storage

--- js/controllers/tea.js
+++ js/controllers/tea.js
@@ -1,6 +1,13 @@
 app.controller('TeaController', ['$scope', 'postmgr', function($scope, postmgr){
+  $scope.teaCategories = []
   postmgr.getTea().then(function(data){
-    console.log(data.data);
-    $scope.items = data.data
+    $scope.items = data.data;
+    data.data.forEach(function(item){
+      item.categories.forEach(function(itemz){
+        if($scope.teaCategories.indexOf(itemz) === -1){
+          $scope.teaCategories.push(itemz)
+        }
+      })
+    })
   })
 }])
--- js/service.js
+++ js/service.js
@@ -3,3 +3,28 @@ app.service('postmgr', ['$http', function($http){
     return $http.get('../tea.json');
   }
 }])
+
+app.service('shopCart', function(){
+  var that = this;
+  localStorage.setItem('shoppingCart', '[]')
+  this.addItem = function(newItem){
+    var currArray = JSON.parse(localStorage.getItem('shoppingCart'));
+    if (that.hasId(currArray, newItem.id)){
+      currArray[that.hasId(currArray, newItem.id)].quantity ++;
+    }else{
+      currArray.push(newItem);
+    }
+    localStorage.setItem('shoppingCart', JSON.stringify(currArray))
+  }
+  this.showItems = function(){
+    return JSON.parse(localStorage.getItem('shoppingCart'))
+  }
+  this.hasId = function(arr, id){
+    arr.forEach(function(item, i){
+      if (item.id === id){
+        return i;
+      };
+    })
+    return false;
+  }
+})
