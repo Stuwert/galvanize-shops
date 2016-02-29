05d01ad24ffdf887a7ae52f754cd31fb5b2e9fc6
Stuart Urback <stuart@urback.net>
2016-02-29 16:14:12 -0700

    Complete general functionality

--- js/controllers/tea.js
+++ js/controllers/tea.js
@@ -1,8 +1,16 @@
-app.controller('TeaController', ['$scope', 'postmgr', function($scope, postmgr){
+app.controller('TeaController', ['$scope', 'postmgr', 'shopCart', function($scope, postmgr, shopCart){
+  $scope.showCart = false;
+  $scope.shopCartTotal = 0;
+  $scope.toggleCart = function(){
+    $scope.showCart = !$scope.showCart;
+  }
+  
   $scope.teaCategories = []
+
   postmgr.getTea().then(function(data){
     $scope.items = data.data;
-    data.data.forEach(function(item){
+    data.data.forEach(function(item, i){
+      $scope.items[i].quantity;
       item.categories.forEach(function(itemz){
         if($scope.teaCategories.indexOf(itemz) === -1){
           $scope.teaCategories.push(itemz)
@@ -10,5 +18,25 @@ app.controller('TeaController', ['$scope', 'postmgr', function($scope, postmgr){
       })
     })
   })
-  $scope.items = getShoppingCart().length;
+
+  $scope.shopCart = shopCart.showItems();
+
+  $scope.addToCart = function(tea){
+    shopCart.addItem(tea);
+    $scope.shopCart = shopCart.showItems();
+    $scope.shopCartTotal = shopCart.total();
+  }
+
+  $scope.updatePrice = function(index, newQuantity){
+    shopCart.updateQuantity(index, newQuantity);
+    $scope.shopCartTotal = shopCart.total();
+  }
+
+  $scope.deleteItem = function(index){
+    shopCart.deleteItem(index);
+    $scope.shopCart = shopCart.showItems();
+    $scope.shopCartTotal = shopCart.total();
+  }
+
+
 }])
--- js/service.js
+++ js/service.js
@@ -9,9 +9,10 @@ app.service('shopCart', function(){
   localStorage.setItem('shoppingCart', '[]')
   this.addItem = function(newItem){
     var currArray = JSON.parse(localStorage.getItem('shoppingCart'));
-    if (that.hasId(currArray, newItem.id)){
-      currArray[that.hasId(currArray, newItem.id)].quantity ++;
+    if (that.hasId(currArray, newItem["_id"]) !== -1){
+      currArray[that.hasId(currArray, newItem["_id"])].quantity += +that.checkQuantity(newItem.quantity);
     }else{
+      newItem.quantity = +that.checkQuantity(newItem.quantity)
       currArray.push(newItem);
     }
     localStorage.setItem('shoppingCart', JSON.stringify(currArray))
@@ -20,11 +21,41 @@ app.service('shopCart', function(){
     return JSON.parse(localStorage.getItem('shoppingCart'))
   }
   this.hasId = function(arr, id){
+    var returnable = -1;
+    if (arr.length < 1) { return -1 };
     arr.forEach(function(item, i){
-      if (item.id === id){
-        return i;
+      if (item["_id"] === id){
+        returnable = i;
       };
     })
-    return false;
+    return returnable;
   }
+  this.checkQuantity = function(quan){
+    if (!quan){
+      return 1;
+    }else {
+      return quan;
+    }
+  }
+  this.total = function(){
+    var total = 0;
+    var arr = JSON.parse(localStorage.getItem('shoppingCart'));
+    arr.forEach(function(item){
+      total += item.quantity * item.price / 100;
+    })
+    return total;
+  }
+
+  this.updateQuantity = function(index, newQuantity){
+    var currArray = JSON.parse(localStorage.getItem('shoppingCart'));
+    currArray[index].quantity = newQuantity;
+    localStorage.setItem('shoppingCart', JSON.stringify(currArray));
+  }
+
+  this.deleteItem = function(index){
+    var currArray = JSON.parse(localStorage.getItem('shoppingCart'));
+    currArray.splice(index, 1);
+    localStorage.setItem('shoppingCart', JSON.stringify(currArray));
+  }
+
 })
